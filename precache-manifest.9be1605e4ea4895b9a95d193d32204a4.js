d21c88f96e797450f1832aa14032467f75f2588a
AntonZubritski <a.zubritski@yandex.ru>
2020-09-20 21:40:06 +0300

    Updates

---
+++ precache-manifest.9be1605e4ea4895b9a95d193d32204a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "410ea2e24b6526388f09429ff5da6670",
+    "url": "/todo-yellow/index.html"
+  },
+  {
+    "revision": "9d65153a352f56a3dc64",
+    "url": "/todo-yellow/static/css/2.8d4cdce2.chunk.css"
+  },
+  {
+    "revision": "e81f03d9f4ac92bf96c1",
+    "url": "/todo-yellow/static/css/main.74aaf242.chunk.css"
+  },
+  {
+    "revision": "9d65153a352f56a3dc64",
+    "url": "/todo-yellow/static/js/2.e9ede792.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/todo-yellow/static/js/2.e9ede792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e81f03d9f4ac92bf96c1",
+    "url": "/todo-yellow/static/js/main.77e66ad3.chunk.js"
+  },
+  {
+    "revision": "8206314901a491d2575d",
+    "url": "/todo-yellow/static/js/runtime-main.ca94e892.js"
+  },
+  {
+    "revision": "82e9431ff6ecf53608aa786508a32cf6",
+    "url": "/todo-yellow/static/media/cursor.82e9431f.cur"
+  }
+]);
